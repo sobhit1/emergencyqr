53f4bb42038dfde12198498fa0a01ba12b5fb90a
sobhit1 <sobhitraghav1092@gmail.com>
2025-02-22 14:15:57 +0530

    fixed error in voicecontrollers

--- src/routes/voiceRoutes.js
+++ src/routes/voiceRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { triggerVoiceSOS } = require("../controllers/voiceSOSController");
+const { triggerVoiceSOS } = require("../controllers/voiceController.js");
 
 const router = express.Router();
 
